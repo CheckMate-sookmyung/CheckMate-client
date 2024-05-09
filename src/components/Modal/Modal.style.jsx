85f1b801819d3287062a3b8214bcb79fb9f3f857
misung.dev <misung.dev@gmail.com>
2024-05-10 02:39:01 +0900

    Style: 모달창 가운데 정렬

--- src/components/Modal/Modal.style.jsx
+++ src/components/Modal/Modal.style.jsx
@@ -2,18 +2,16 @@ import styled from 'styled-components';
 
 export const ModalLayout = styled.div`
   position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: 100;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 20px;
-  width: 100%;
+  width: 800px;
   height: 460px;
-  border: 1px solid black; /* 추후 삭제하기 */
   border-radius: 12px;
   background-color: #ffffff;
 `;
