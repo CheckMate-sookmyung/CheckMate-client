c99b41c3fd590bd5fa713a955eb559ff7d96efe8
misung.dev <misung.dev@gmail.com>
2024-07-07 06:08:51 +0900

    feat: #113 참석자 관리 페이지 일부 구현

--- src/pages/DashboardPage/DashboardAttendee.jsx
+++ src/pages/DashboardPage/DashboardAttendee.jsx
@@ -1,5 +1,65 @@
+import { useState } from 'react';
 import * as S from './DashboardAttendee.style';
 
 export default function DashboardAttendee() {
-  return <S.DashboardAttendee></S.DashboardAttendee>;
+  const [activeTab, setActiveTab] = useState(1);
+
+  return (
+    <S.DashboardAttendee>
+      <S.TopContainer>
+        <S.Title>참석자 관리</S.Title>
+        <S.ButtonContainer>
+          <S.Btn>참석자 데이터 다운로드</S.Btn>
+        </S.ButtonContainer>
+      </S.TopContainer>
+
+      <S.SearchContainer>
+        <S.SearchBox></S.SearchBox>
+        <S.CheckIn></S.CheckIn>
+      </S.SearchContainer>
+
+      <S.TabContainer>
+        <S.TabBar>
+          {[1, 2, 3].map((tab) => (
+            <S.Tab
+              key={tab}
+              active={activeTab === tab}
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab}회
+            </S.Tab>
+          ))}
+        </S.TabBar>
+
+        <S.EditBtn>출석 여부 수정</S.EditBtn>
+      </S.TabContainer>
+
+      {/* 참석자 리스트 */}
+      <S.TableContainer>
+        <S.TableTitle>
+          <S.Table>
+            <thead>
+              <tr>
+                <S.TableHeader>
+                  <input type="checkbox" />
+                </S.TableHeader>
+                <S.TableHeader>학과</S.TableHeader>
+                <S.TableHeader>이름</S.TableHeader>
+                <S.TableHeader>학번</S.TableHeader>
+                <S.TableHeader>학년</S.TableHeader>
+                <S.TableHeader>휴대폰 번호</S.TableHeader>
+                <S.TableHeader>이메일 주소</S.TableHeader>
+                <S.TableHeader>출석 여부</S.TableHeader>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <S.TableData colSpan="9">결과가 없습니다.</S.TableData>
+              </tr>
+            </tbody>
+          </S.Table>
+        </S.TableTitle>
+      </S.TableContainer>
+    </S.DashboardAttendee>
+  );
 }
--- src/pages/DashboardPage/DashboardAttendee.style.jsx
+++ src/pages/DashboardPage/DashboardAttendee.style.jsx
@@ -1,7 +1,176 @@
 import styled from 'styled-components';
 
 export const DashboardAttendee = styled.div`
-  background: rgb(242, 243, 245);
+  background: #f2f3f5;
   min-height: 100%;
   padding: 76px;
+`;
+
+// 행사 타이틀 + 버튼
+export const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+export const Btn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #ebedf0;
+  padding: 9px 18px;
+  font-weight: 600;
+  font-size: 14px;
+  height: 30px;
+  color: #2253ff;
+  cursor: pointer;
+  transition:
+    background 0.3s ease,
+    box-shadow 0.3s ease,
+    transform 0.3s ease;
+
+  &:hover {
+    background: #4d74ff;
+    color: #fff;
+  }
+`;
+
+// 검색창
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const SearchBox = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  border: 1px solid white;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 10px;
+  width: 420px;
+  height: 40px;
+  background-color: #ffffff;
+`;
+
+export const CheckIn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 240px;
+  background-color: white;
+  padding-left: 30px;
+  padding-right: 30px;
+  border-radius: 7px;
+  height: 40px;
+`;
+
+// 탭정보
+export const TabContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TabBar = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 20px 0;
+`;
+
+export const Tab = styled.button`
+  padding: 10px 20px;
+  border: 1px solid ${(props) => (props.active ? '#4e75ff' : '#4e75ff')};
+  border-radius: 5px;
+  background-color: ${(props) => (props.active ? '#4e75ff;' : '#fff')};
+  color: ${(props) => (props.active ? 'white' : 'black')};
+  cursor: pointer;
+
+  &:hover {
+    background-color: #4e75ff;
+    color: white;
+  }
+`;
+
+// 출석 수정 버튼
+export const EditBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  height: 30px;
+  padding: 0 10px;
+  margin: 0 5px;
+  border-radius: 7px;
+  border: 1px solid #ebedf0;
+  cursor: pointer;
+  min-width: 84px;
+  font-size: 12px;
+  font-weight: 600;
+
+  &:hover {
+    border: 1px solid #4e75ff;
+  }
+`;
+
+// 행사 정보
+export const TableContainer = styled.div`
+  width: 100%;
+`;
+
+export const TableTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  position: relative;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+export const TableHeader = styled.th`
+  padding: 16px;
+  text-align: left;
+  font-size: 12px;
+  font-weight: bold;
+  color: #909499;
+  border-bottom: 1px solid #ccc;
+  white-space: nowrap;
+  overflow: hidden;
+
+  &:first-child {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const TableData = styled.td`
+  padding: 16px;
+  text-align: center;
+  font-size: 14px;
+  color: #555;
+  white-space: nowrap;
+  overflow: hidden;
+
+  &:first-child {
+    text-align: left;
+  }
 `;
