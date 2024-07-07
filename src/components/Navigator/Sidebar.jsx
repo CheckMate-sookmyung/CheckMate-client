450f5e7ab8fce0b923982ceb1cd3fdfc10cc0526
misung.dev <misung.dev@gmail.com>
2024-07-07 18:05:38 +0900

    refactor: #107 반복되는 항목 리팩토링

--- src/components/Navigator/Sidebar.jsx
+++ src/components/Navigator/Sidebar.jsx
@@ -10,62 +10,58 @@ import {
 } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 
+const menuItems = [
+  { to: '/event/dashboard', icon: <FaTableList />, text: '대시보드' },
+  {
+    to: '/event/dashboard/info',
+    icon: <FaCircleInfo />,
+    text: '행사 기본 정보',
+  },
+  {
+    to: '/event/dashboard/info',
+    icon: <FaImages />,
+    text: '행사 상세와 사진',
+  },
+  {
+    to: '/event/dashboard/email',
+    icon: <FaEnvelope />,
+    text: '이메일 예약 발송',
+  },
+  { to: '/event/dashboard/attendee', icon: <FaUsers />, text: '참석자 관리' },
+  { to: '/event/dashboard/attendee', icon: <FaChartPie />, text: '통계' },
+];
+
+function MenuItem({ to, icon, text, active }) {
+  return (
+    <S.MenuItem className={active ? 'active' : ''}>
+      <S.Icon>{icon}</S.Icon>
+      <S.Text>
+        <Link to={to}>{text}</Link>
+      </S.Text>
+    </S.MenuItem>
+  );
+}
+
 export default function Sidebar() {
   return (
     <S.Sidebar>
       <S.MenuWrapper>
-        <S.MenuItem>
-          <S.Icon>
-            <FaTableList />
-          </S.Icon>
-          <S.Text>
-            <Link to="/event/dashboard">대시보드</Link>
-          </S.Text>
-        </S.MenuItem>
-        <S.MenuItem>
-          <S.Icon>
-            <FaCircleInfo />
-          </S.Icon>
-          <S.Text>
-            <Link to="/event/dashboard/info"> 행사 기본 정보</Link>
-          </S.Text>
-        </S.MenuItem>
-        <S.MenuItem>
-          <S.Icon>
-            <FaImages />
-          </S.Icon>
-          <S.Text>
-            <Link to="/event/dashboard/info">행사 상세와 사진</Link>
-          </S.Text>
-        </S.MenuItem>
-      </S.MenuWrapper>
-      <S.MenuWrapper>
-        <S.MenuItem>
-          <S.Icon>
-            <FaEnvelope />
-          </S.Icon>
-          <S.Text>
-            <Link to="/event/dashboard/email"> 이메일 예약 발송</Link>
-          </S.Text>
-        </S.MenuItem>
-        <S.MenuItem className="active">
-          <S.Icon>
-            <FaUsers />
-          </S.Icon>
-          <S.Text>
-            <Link to="/event/dashboard/attendee">참석자 관리</Link>
-          </S.Text>
-        </S.MenuItem>
+        {menuItems.slice(0, 3).map((item) => (
+          <MenuItem key={item.to} {...item} />
+        ))}
       </S.MenuWrapper>
 
-      <S.MenuItem>
-        <S.Icon>
-          <FaChartPie />
-        </S.Icon>
-        <S.Text>
-          <Link to="/event/dashboard/attendee">통계</Link>
-        </S.Text>
-      </S.MenuItem>
+      <S.MenuWrapper>
+        {menuItems.slice(3, 5).map((item) => (
+          <MenuItem key={item.to} {...item} />
+        ))}
+      </S.MenuWrapper>
+
+      <S.MenuWrapper>
+        {menuItems.slice(5).map((item) => (
+          <MenuItem key={item.to} {...item} />
+        ))}
+      </S.MenuWrapper>
     </S.Sidebar>
   );
 }
