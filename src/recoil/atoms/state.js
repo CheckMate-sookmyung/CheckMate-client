167e01dfa6310a704dbe424aa5308408f59bbeae
Jeonghyun <limithyunii@gmail.com>
2024-07-08 08:19:17 +0900

    feat:recoil-persist 추가

--- src/recoil/atoms/state.js
+++ src/recoil/atoms/state.js
@@ -1,6 +1,13 @@
+import { recoilPersist } from 'recoil-persist';
 import { atom, useRecoilState } from 'recoil';
+
+const { persistAtom } = recoilPersist({
+  key: 'persistEventID',
+  storage: localStorage,
+});
 
 export const eventIDState = atom({
   key: 'EVENT_ID',
   default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
