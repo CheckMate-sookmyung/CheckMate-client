612d1f15f91f6f7ec59905ee8031ef37c59bf863
Jeonghyun <limithyunii@gmail.com>
2024-07-10 15:46:28 +0900

    feat: 등록을 위한 state 추가

--- src/recoil/atoms/state.js
+++ src/recoil/atoms/state.js
@@ -1,13 +1,37 @@
 import { recoilPersist } from 'recoil-persist';
-import { atom, useRecoilState } from 'recoil';
+import { atom } from 'recoil';
 
 const { persistAtom } = recoilPersist({
   key: 'persistEventID',
-  storage: localStorage,
+  storage: sessionStorage,
 });
 
 export const eventIDState = atom({
   key: 'EVENT_ID',
   default: 0,
   effects_UNSTABLE: [persistAtom],
+});
+
+export const { persistEvent } = recoilPersist({
+  key: 'persistRegister',
+  storage: sessionStorage,
+});
+
+export const RegisterState = atom({
+  key: 'RegisterInfo',
+  default: {
+    eventType: '',
+    eventTitle: '',
+    eventDetail: '',
+    eventImage: '',
+    attendanceListFile: '',
+    minCompletionTimes: '',
+    eventSchedules: [{ eventDate: '', eventStartTime: '', eventEndTime: '' }],
+  },
+  effects_UNSTABLE: [persistEvent],
+});
+
+export const RegisterStep = atom({
+  key: 'RegisterStep',
+  default: 1,
 });
