d02b871916b4052db1c70a9026be111709457e07
Jeonghyun <limithyunii@gmail.com>
2024-07-10 15:44:54 +0900

    modify: EventList Style 분리 및 일부 수정

--- src/pages/EventList/EventList.jsx
+++ src/pages/EventList/EventList.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import BackgroundPage from '../../components/Background/BackgroundPage';
+import * as S from './EventListStyle';
 import { USER_ID } from '../../constants';
 import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../../axios';
-import { BREAKPOINTS } from '../../styles';
 import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { eventIDState } from '../../recoil/atoms/state';
 
-export default function CurrentEvent() {
+const EventList = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -38,22 +36,22 @@ export default function CurrentEvent() {
   }, []);
 
   return (
-    <BackgroundPage title={'진행 중인 행사'}>
-      <EventCardList>
-        {events.map((event) => (
-          <EventCard
-            key={event.id}
-            id={event.id}
-            title={event.title}
-            startDate={event.startDate}
-            endDate={event.endDate}
-            poster={event.poster}
-          />
-        ))}
-      </EventCardList>
-    </BackgroundPage>
+    <S.EventCardList>
+      {events.map((event) => (
+        <EventCard
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          startDate={event.startDate}
+          endDate={event.endDate}
+          poster={event.poster}
+        />
+      ))}
+    </S.EventCardList>
   );
-}
+};
+
+export default EventList;
 
 const EventCard = ({ id, title, poster, startDate, endDate }) => {
   const setContent = useSetRecoilState(eventIDState);
@@ -61,7 +59,7 @@ const EventCard = ({ id, title, poster, startDate, endDate }) => {
 
   const handleDetail = () => {
     setContent(id);
-    navigate('/event/detail');
+    navigate(`/event/dashboard`);
   };
 
   const attendanceCheck = (event) => {
@@ -70,97 +68,18 @@ const EventCard = ({ id, title, poster, startDate, endDate }) => {
   };
 
   return (
-    <CardWrapper onClick={handleDetail}>
-      <EventImgWrapper>
-        <EventImg src={poster} alt="event_poster" />
-      </EventImgWrapper>
-      <EventTitle>{title}</EventTitle>
-      <EventDate>
+    <S.CardWrapper onClick={handleDetail}>
+      <S.EventImgWrapper>
+        <S.EventImg src={poster} alt="event_poster" />
+      </S.EventImgWrapper>
+      <S.EventTitle>{title}</S.EventTitle>
+      <S.EventDate>
         <p>진행 일정</p>
-        <CardDay>{endDate ? `${startDate} ~ ${endDate}` : startDate}</CardDay>
-      </EventDate>
-      <BlueButton onClick={attendanceCheck}>출석 체크</BlueButton>
-    </CardWrapper>
+        <S.CardDay>
+          {endDate ? `${startDate} ~ ${endDate}` : startDate}
+        </S.CardDay>
+      </S.EventDate>
+      <S.BlueButton onClick={attendanceCheck}>출석 체크</S.BlueButton>
+    </S.CardWrapper>
   );
 };
-
-const CardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 12px;
-  box-shadow:
-    rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
-    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
-  border-radius: 8px;
-  background-color: white;
-  cursor: pointer;
-  &:hover {
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-  }
-`;
-
-const EventImgWrapper = styled.div`
-  display: flex;
-  overflow: hidden;
-  height: 260px;
-  justify-content: center;
-`;
-
-const EventImg = styled.img`
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 420 / 594;
-  object-fit: cover;
-`;
-
-const EventCardList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 32px;
-  width: 100%;
-  padding: 50px 20px;
-
-  @media (max-width: ${BREAKPOINTS[2]}px) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 20px;
-  }
-  @media (max-width: ${BREAKPOINTS[1]}px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: ${BREAKPOINTS[0]}px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
-
-const EventTitle = styled.p`
-  font-size: 18px;
-  font-weight: 700;
-  margin: 20px 0 10px;
-`;
-
-const EventDate = styled.div`
-  display: flex;
-  gap: 12px;
-  margin: 10px 0px;
-`;
-
-const CardDay = styled.p`
-  font-size: 16px;
-  font-weight: 700;
-`;
-
-const BlueButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 40px;
-  margin: 10px 0;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  background: linear-gradient(to right, #0a2c83, #1f5fa9);
-  cursor: pointer;
-  font-size: 16px;
-`;
