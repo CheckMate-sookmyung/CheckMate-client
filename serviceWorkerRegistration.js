d863208b5c547ff761fe8edd164993343e02ada7
Jeonghyun <limithyunii@gmail.com>
2024-07-22 12:58:39 +0900

    modify: URL 변수 지정

--- serviceWorkerRegistration.js
+++ serviceWorkerRegistration.js
@@ -1,3 +1,5 @@
+const PUBLIC_URL = 'https://www.checkmate.pe.kr/';
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     window.location.hostname === '[::1]' ||
@@ -8,13 +10,13 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service/service-worker.js`;
+      const swUrl = `${PUBLIC_URL}/service/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
