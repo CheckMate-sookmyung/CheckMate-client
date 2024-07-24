832ea41840a9ee609b02b0dba01cd86e43dd2ea9
misung.dev <misung.dev@gmail.com>
2024-07-24 21:06:48 +0900

    #172 feat: eventTarget이 외부인인 경우 휴대폰 번호 뒷 4자리로 입력받는 코드 추가

--- src/pages/AttendancePage/AttendanceStudentIdPage.jsx
+++ src/pages/AttendancePage/AttendanceStudentIdPage.jsx
@@ -16,14 +16,19 @@ const AttendanceStudentIdPage = () => {
   const [eventDate, setEventDate] = useState('');
   const [isAlreadyCompleted, setIsAlreadyCompleted] = useState(false);
   const [isNoMatch, setIsNoMatch] = useState(false);
+  const [eventTarget, setEventTarget] = useState('INTERNAL');
   const EVENT_ID = useRecoilValue(eventIDState);
 
   const { setSessionStorage } = useSessionStorages();
 
   const studentId = Array.from({ length: 7 }, (_, index) => index + 1);
+  const phoneId = Array.from({ length: 4 }, (_, index) => index + 1);
   const dialList = Array.from({ length: 9 }, (_, index) => index + 1);
 
-  const isSevenDigits = enteredDials.length === 7;
+  const isSevenDigits =
+    eventTarget === 'INTERNAL'
+      ? enteredDials.length === 7
+      : enteredDials.length === 4;
   const isConfirmEnabled = isSevenDigits;
 
   const getAttendanceCheck = async (params) => {
@@ -51,7 +56,7 @@ const AttendanceStudentIdPage = () => {
 
         if (!data) {
           setIsNoMatch(true);
-          alert('일치하는 학번이 없습니다');
+          alert('일치하는 정보가 없습니다');
         } else if (data.isAlreadyCompleted) {
           setIsAlreadyCompleted(true);
           alert('이미 출석을 완료하였습니다');
@@ -71,13 +76,16 @@ const AttendanceStudentIdPage = () => {
       } catch (error) {
         setEnteredDials([]);
         if (error.response && error.response.status === 404) {
-          alert('일치하는 학번이 없습니다');
+          alert('일치하는 정보가 없습니다');
         } else {
           alert('API 에러 발생');
         }
       }
     } else {
-      if (enteredDials.length < 7 && dial !== '서명하러 가기') {
+      if (
+        enteredDials.length < (eventTarget === 'INTERNAL' ? 7 : 4) &&
+        dial !== '서명하러 가기'
+      ) {
         setEnteredDials([...enteredDials, dial]);
       }
     }
@@ -91,8 +99,10 @@ const AttendanceStudentIdPage = () => {
         const response = await axiosInstance.get(
           `/api/v1/events/${USER_ID}/${EVENT_ID}`,
         );
-        setEventTitle(response.data.eventTitle);
-        setEventDate(response.data.eventSchedules[0].eventDate); // 첫 번째 스케줄의 날짜를 가져옴
+        const eventData = response.data;
+        setEventTitle(eventData.eventTitle);
+        setEventDate(eventData.eventSchedules[0].eventDate);
+        setEventTarget(eventData.eventTarget);
       } catch (error) {
         console.error('이벤트 정보를 가져오는 중 에러 발생:', error);
       }
@@ -104,9 +114,13 @@ const AttendanceStudentIdPage = () => {
   return (
     <S.Container>
       <AttendanceHeader eventTitle={eventTitle} />
-      <S.Title>학번을 입력해주세요.</S.Title>
+      <S.Title>
+        {eventTarget === 'INTERNAL'
+          ? '학번을 입력해주세요.'
+          : '휴대폰 번호 뒷자리 4자리를 입력해주세요.'}
+      </S.Title>
       <S.StudentIdContainer>
-        {studentId.map((index) => (
+        {(eventTarget === 'INTERNAL' ? studentId : phoneId).map((index) => (
           <S.StudentId key={index}>{enteredDials[index - 1] || ''}</S.StudentId>
         ))}
       </S.StudentIdContainer>
