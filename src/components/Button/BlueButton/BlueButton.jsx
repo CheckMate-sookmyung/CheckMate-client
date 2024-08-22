b1d83e960be3b832ff3d963582ff65a89aedf8b6
misung.dev <misung.dev@gmail.com>
2024-08-22 11:29:37 +0900

    #197 [MODIFY] 버튼에 props 속성 추가

--- src/components/Button/BlueButton/BlueButton.jsx
+++ src/components/Button/BlueButton/BlueButton.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import * as S from './BlueButtonStyle';
 
-const BlueButton = ({ label, onClick, size = 'large' }) => {
+const BlueButton = ({
+  label,
+  onClick,
+  size = 'large',
+  backgroundColor = '#2f7cef',
+  textColor = '#fff',
+}) => {
   return (
-    <S.Button onClick={onClick} size={size}>
+    <S.Button
+      onClick={onClick}
+      size={size}
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+    >
       {label}
     </S.Button>
   );
