666bb85a3b700a00de10c17a76426f69cc73f62a
misung.dev <misung.dev@gmail.com>
2024-08-22 22:33:49 +0900

    #211 modify: footer에 너비 1100px로 지정 및 리팩토링

--- src/components/Footer/Footer.Style.js
+++ src/components/Footer/Footer.Style.js
@@ -1,117 +1,88 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  position: relative;
+  display: flex;
   background-color: #f8f8f8;
   height: 256px;
-  overflow: hidden;
-  text-align: left;
-  font-size: 14px;
-  color: #323232;
-  font-family: Pretendard;
 `;
 
-export const Child = styled.div`
-  position: absolute;
-  top: calc(50% - 127.5px);
-  left: calc(50% - 720px);
-  width: 1440px;
-  height: 256px;
+export const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  max-width: 1100px;
+  padding: 64px 0;
+  margin: 0 auto;
+  gap: 140px;
 `;
 
-export const Category = styled.div`
-  position: absolute;
-  top: 64px;
-  left: 352px;
-  font-weight: 600;
+export const Logo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 100%;
 `;
 
-export const Copyright = styled.div`
-  position: absolute;
-  top: 155px;
-  left: 352px;
-  font-weight: 500;
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
 `;
 
-export const FirstDiv = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-`;
-
-export const Address = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 89px;
-`;
-
-export const Vector404Stroke = styled.img`
-  position: absolute;
-  top: 0px;
-  left: 80.5px;
-  width: 1px;
-  height: 14px;
-`;
-
-export const Parent = styled.div`
-  position: absolute;
-  top: 133px;
-  left: 352px;
-  width: 357px;
-  height: 14px;
-  font-size: 12px;
-`;
-
-export const NameBox = styled.div`
-  position: relative;
-`;
-
-export const Vector401Stroke = styled.img`
-  width: 1px;
-  position: relative;
-  height: 14px;
+export const GroupWrapper = styled.div`
+  display: flex;
+  gap: 100px;
 `;
 
 export const Group = styled.div`
-  position: absolute;
-  top: 89px;
-  left: 352px;
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
+  flex-direction: column;
   gap: 8px;
-  font-size: 12px;
 `;
 
-export const Vector403Stroke = styled.img`
-  position: absolute;
-  top: 1px;
-  left: 86.5px;
-  width: 1px;
-  height: 14px;
+export const Category = styled.h4`
+  color: var(--Black-2, #323232);
+  font-weight: 600;
 `;
 
-export const SecondBox = styled.div`
-  position: absolute;
-  top: 1px;
-  left: 95px;
+export const MemberList = styled.ul`
+  display: flex;
 `;
 
-export const Vector403StrokeParent = styled.div`
-  position: absolute;
-  top: 88px;
-  left: 856px;
-  width: 207px;
-  height: 15px;
-  font-size: 12px;
+export const MemberItem = styled.li`
+  &:not(:last-of-type) {
+    &::after {
+      content: '|';
+      color: #6b7684;
+      margin: 0 8px;
+    }
+  }
 `;
 
-export const Logo = styled.img`
-  position: absolute;
-  top: 64.5px;
-  left: 100px;
-  width: 89px;
-  height: 32px;
+export const Member = styled.span`
+  color: var(--Black-2, #323232);
 `;
+
+// copyright
+export const CopyrightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const University = styled.div`
+  color: var(--Black-2, #323232);
+`;
+
+export const UniversityName = styled.span`
+  &::after {
+    content: '|';
+    color: var(--Black-2, #323232);
+    margin: 0 8px;
+  }
+`;
+
+export const Address = styled(Member)``;
+
+export const Copyright = styled(Category)``;
