4215a963e2909a12d360630aa99336427ea6cdc0
misung.dev <misung.dev@gmail.com>
2024-09-06 18:21:12 +0900

    #250 design: BlueButton 컴포넌트 사이즈 조정

--- src/pages/RegisterPage/RegisterComponents/Button/BlueButton.jsx
+++ src/pages/RegisterPage/RegisterComponents/Button/BlueButton.jsx
@@ -1,45 +1,29 @@
 import styled from 'styled-components';
 
-import React from 'react';
-
 export default function BlueButton({ contents }) {
   return (
-    <>
-      <Container>
-        <Wrapper>
-          <Frame>{contents}</Frame>
-        </Wrapper>
-      </Container>
-    </>
+    <Container>
+      <Frame>{contents}</Frame>
+    </Container>
   );
 }
 
 const Container = styled.div`
-  position: relative;
-  font-weight: 500;
-`;
-
-const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
-  justify-content: flex-start;
 `;
 
-const Frame = styled.div`
-  position: relative;
+const Frame = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 30px;
   background-color: #edf5ff;
   border: 1px solid #aecfff;
   box-sizing: border-box;
+  padding: 8px 18px;
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-start;
-  padding: 12px 26px;
-  text-align: left;
-  font-size: 20px;
   color: #2f7cef;
-  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
 `;
