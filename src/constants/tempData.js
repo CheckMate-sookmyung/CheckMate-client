d3207e4e00adbc9540181035e3921ecacdda4d66
misung.dev <misung.dev@gmail.com>
2024-09-06 12:26:31 +0900

    #227 modify: user id 수정

--- src/constants/tempData.js
+++ src/constants/tempData.js
@@ -1,8 +1,8 @@
 // 테스트 코드
-// export const USER_ID = 100;
+export const USER_ID = 100;
 
 // APPS
-export const USER_ID = 300;
+// export const USER_ID = 300;
 
 // 소중대
 // export const USER_ID = 500;
