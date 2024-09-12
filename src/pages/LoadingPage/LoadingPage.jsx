e024df794de69d1a4f638b1f4587a590d58ccb5a
Jeonghyun <limithyunii@gmail.com>
2024-09-12 10:40:56 +0900

    feat: #273 회원가입/로그인 api 로직 연결 위한 페이지 구현

--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,30 +1,57 @@
 import { axiosInstance } from '@/axios';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as S from './LoadingPage.style';
 
 const LoadingPage = () => {
   const navigate = useNavigate();
 
-  // 이미 가입한 유저일 시 : 메인 페이지로 이동
   const handleHome = () => {
     navigate('/');
-    window.location.reload();
   };
 
   const params = new URLSearchParams(window.location.search);
   const code = params.get('code');
 
   const handleLoginPost = async (code) => {
-    const data = {
-      code: code,
-    };
     try {
-      const res = await axiosInstance.post('/api/v2/signup', data);
-      const accessToken = res.data.accessToken;
-      localStorage.setItem('bagtoken', accessToken);
+      const tokenResponse = await axiosInstance.get(
+        `/api/v1/code?code=${code}`,
+      );
+      const accessToken = tokenResponse.data.access_token;
+      localStorage.setItem('accessToken', accessToken);
+
+      if (!accessToken) {
+        alert('Access token not found');
+      }
+
+      const userResponse = await axiosInstance.get(
+        `/api/v1/login?accessToken=${accessToken}`,
+      );
+      const { id, socialId, name, email, refreshToken, isNewMember } =
+        userResponse.data;
+
+      localStorage.setItem('id', id);
+      localStorage.setItem('email', email);
+      localStorage.setItem('name', name);
+      localStorage.setItem('socialId', socialId);
+      localStorage.setItem('refreshToken', refreshToken);
+
+      if (isNewMember) {
+        const memberInfoRequest = { email, name, socialId };
+        const signupResponse = await axiosInstance.post(
+          `/api/v1/signup`,
+          memberInfoRequest,
+        );
+        const { id } = signupResponse.data;
+
+        localStorage.setItem('id', id);
+      }
       handleHome();
+      window.location.reload();
     } catch (error) {
-      console.log(error);
+      console.error('Login failed:', error);
+      handleHome();
     }
   };
 
@@ -32,14 +59,16 @@ const LoadingPage = () => {
     if (code) {
       handleLoginPost(code);
     } else {
-      console.log('로그인 재시도하세요.');
+      alert('authorization code를 찾을 수 없습니다.');
+      handleHome();
     }
   }, [code, navigate]);
 
   return (
-    <div>
-      <h1>로그인중입니다...</h1>
-    </div>
+    <S.LoadingContainer>
+      <S.Spinner />
+      <S.LoadingText>로그인 중입니다...</S.LoadingText>
+    </S.LoadingContainer>
   );
 };
 
