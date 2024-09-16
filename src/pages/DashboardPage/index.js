4c42b5255fa97e93d814e00d853197221f8dc375
misung.dev <misung.dev@gmail.com>
2024-09-17 02:51:48 +0900

    #290 feat: 설문조사 링크 발송 페이지 생성

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -1,5 +1,6 @@
 export { default as DashboardPage } from './DashboardPage';
 export { default as DashboardInfoPage } from './DashboardInfoPage/DashboardInfoPage';
 export { default as DashboardEmailPage } from './DashboardEmailPage/DashboardEmailPage';
+export { default as DashboardSurveyPage } from './DashboardSurveyPage/DashboardSurveyPage';
 export { default as DashboardAttendeePage } from './DashboardAttendeePage/DashboardAttendeePage';
 export { default as DashboardStatisticPage } from './DashboardStatisticPage/DashboardStatisticPage';
