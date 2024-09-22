b9bf6fa408cc6fd490ef41ef9021f0a08d0e2c93
Jeonghyun <limithyunii@gmail.com>
2024-09-22 13:45:39 +0900

    #320 feat: 리디렉션 주소 변경

--- src/services/privateRoute/PrivateRoute.jsx
+++ src/services/privateRoute/PrivateRoute.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const isAuthenticated = !!sessionStorage.getItem('accessToken');
+  const isAuthenticated = sessionStorage.getItem('accessToken');
+  const location = useLocation();
 
-  return isAuthenticated ? (
-    <Component {...rest} />
-  ) : (
-    <Navigate
-      to={{
-        pathname: '/',
-        state: { from: rest.location },
-      }}
-    />
-  );
+  if (isAuthenticated) {
+    return <Component {...rest} />;
+  } else {
+    window.location.href = `${process.env.REACT_APP_GOOGLE_OAUTH_BASE_URL}/o/oauth2/auth?client_id=${process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_CLIENT_BASE_URL}/loading&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
+    return null;
+  }
 }
 
 export default PrivateRoute;
