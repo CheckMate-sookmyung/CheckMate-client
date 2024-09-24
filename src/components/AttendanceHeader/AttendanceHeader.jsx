7f46025fb69ded33a79d9519b89b0265a1d4cf16
misung.dev <misung.dev@gmail.com>
2024-09-24 23:26:28 +0900

    #352 feat: x아이콘 누르면 참석자 관리 페이지로 이동

--- src/components/AttendanceHeader/AttendanceHeader.jsx
+++ src/components/AttendanceHeader/AttendanceHeader.jsx
@@ -1,13 +1,16 @@
 import * as S from './AttendanceHeader.style';
 import PropTypes from 'prop-types';
 import { FiX } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const AttendanceHeader = ({ eventTitle }) => {
   return (
     <S.Container>
-      <S.CloseIconAnchor onClick={() => window.history.back()}>
-        <FiX color="#000" />
-      </S.CloseIconAnchor>
+      <Link to="/events/dashboard/attendee">
+        <S.CloseIconAnchor>
+          <FiX color="#000" />
+        </S.CloseIconAnchor>
+      </Link>
       <S.ContentContainer>
         <S.Title>
           <em>{eventTitle}</em>
