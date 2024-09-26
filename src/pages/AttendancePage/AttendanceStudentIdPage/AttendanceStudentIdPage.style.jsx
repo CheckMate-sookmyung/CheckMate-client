3a78c1aa1229da1a14901590c2db40ec82edffe8
misung.dev <misung.dev@gmail.com>
2024-09-27 03:32:37 +0900

    #369 feat: 모달창 및 동일 번호 가지고 있는 사람 목록 노출하는 기능 구현

--- src/pages/AttendancePage/AttendanceStudentIdPage/AttendanceStudentIdPage.style.jsx
+++ src/pages/AttendancePage/AttendanceStudentIdPage/AttendanceStudentIdPage.style.jsx
@@ -155,3 +155,139 @@ export const GoToSignBtn = styled.div`
     font-size: 20px;
   }
 `;
+
+// 모달
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  justify-content: space-between;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 66px 40px;
+  width: 800px;
+  height: auto;
+  border-radius: 12px;
+  background-color: #ffffff;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    width: 500px;
+  }
+
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    width: 300px;
+    padding: 30px 16px 20px;
+  }
+`;
+
+export const ModalTitle = styled.h2`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 34px;
+  font-size: 32px;
+  font-weight: 600;
+  color: var(--blue-0, #2f7cef);
+  line-height: 1.2;
+  word-break: keep-all;
+  text-align: center;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    font-size: 28px;
+    margin-bottom: 30px;
+  }
+
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    font-size: 26px;
+    margin-bottom: 26px;
+  }
+`;
+
+export const AttendeeList = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 28px;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    gap: 20px;
+  }
+
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    gap: 14px;
+  }
+`;
+
+export const AttendeeItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px 38px;
+  border-radius: 10px;
+  border: 1px solid var(--blue-4, #accdff);
+  background: #f4f8ff;
+  font-size: 16px;
+  gap: 26px;
+  cursor: pointer;
+
+  @media (max-width: ${BREAKPOINTS[1]}px) {
+    padding: 10px 18px;
+  }
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    padding: 10px 18px;
+    gap: 10px;
+  }
+`;
+
+export const AttendeeName = styled.span`
+  color: var(--Black-2, #323232);
+  font-size: 24px;
+  font-weight: 500;
+  padding: 0 10px;
+
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    font-size: 20px;
+  }
+`;
+
+export const AttendeeInfo = styled.span`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-radius: 30px;
+  border: 1px solid #aecfff;
+  padding: 12px 26px;
+  background: var(--White, #fff);
+  gap: 8px;
+  color: var(--blue-0, #2f7cef);
+  font-size: 20px;
+  font-weight: 500;
+
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    padding: 8px 16px;
+    font-size: 20px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+export const CloseButton = styled.button`
+  margin-top: 36px;
+  border-radius: 10px;
+  padding: 20px 28px;
+  background: var(--LG-3, #f2f2f2);
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--DG-2, #818181);
+  @media (max-width: ${BREAKPOINTS[0]}px) {
+    padding: 12px 20px;
+    font-size: 16px;
+  }
+`;
